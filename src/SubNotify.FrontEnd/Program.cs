e65b894759eebb262bc1bdb3dc823b10613974d8
Mark Strendin <mark.strendin@lskysd.ca>
2024-07-26 15:01:50 -0600

    Add OIDC support

--- src/SubNotify.FrontEnd/Program.cs
+++ src/SubNotify.FrontEnd/Program.cs
@@ -1,10 +1,75 @@
 using SubNotify.FrontEnd.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// My notes on how to set up OIDC in .Net 8: https://github.com/MarkStrendin/BlazorDotNet8OIDC
+
+// Add blazor authentication
+const string MS_OIDC_SCHEME = "MicrosoftOidc";
+builder.Services.AddAuthentication(MS_OIDC_SCHEME)
+    .AddOpenIdConnect(MS_OIDC_SCHEME, oidcOptions =>
+    {
+        // Pull in configuration, so we can get the OIDC info
+        IConfiguration Configuration = new ConfigurationBuilder()
+                    .AddEnvironmentVariables()
+                    .AddUserSecrets<Program>()
+                    .Build();
+
+        oidcOptions.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;        
+        oidcOptions.Scope.Add("openid");
+        oidcOptions.Scope.Add("profile");
+        oidcOptions.Scope.Add(OpenIdConnectScope.OpenIdProfile);
+        oidcOptions.GetClaimsFromUserInfoEndpoint = true;
+        oidcOptions.Authority = Configuration["OIDC:Authority"];
+        oidcOptions.ClientId = Configuration["OIDC:ClientId"];
+        oidcOptions.ClientSecret = Configuration["OIDC:ClientSecret"];
+        oidcOptions.SaveTokens = true;
+        oidcOptions.ResponseType = OpenIdConnectResponseType.Code;
+        oidcOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            NameClaimType = "name",
+            RoleClaimType = "groups",
+            ValidateIssuer = true
+        };
+        oidcOptions.Events = new OpenIdConnectEvents
+        {
+            OnAccessDenied = context =>
+            {
+                context.HandleResponse();
+                context.Response.Redirect("/");
+                return Task.CompletedTask;
+            }
+        };
+    })
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+
+builder.Services.AddAuthorization();
+builder.Services.AddCascadingAuthenticationState();
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add services for dependency injection
+// example:
+//  services.AddScoped<IRegistrationRepository<School>, MongoRepository<School>>();
 
 var app = builder.Build();
 
@@ -17,10 +82,15 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
+
+app.MapGroup("/authentication").MapLoginAndLogout();
 
 app.Run();

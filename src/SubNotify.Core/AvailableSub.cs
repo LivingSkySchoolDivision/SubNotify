a9860e6c978f23d24b1fc65d217170fc9266ad1e
Mark Strendin <mark.strendin@lskysd.ca>
2024-10-28 13:11:30 -0600

    Add some help text, and remove an unused field (sub email) (#3)

--- src/SubNotify.Core/AvailableSub.cs
+++ src/SubNotify.Core/AvailableSub.cs
@@ -6,7 +6,6 @@ public class AvailableSub : IGUIDable
     public bool IsEnabled { get; set; } = true;
     public string GivenName { get; set; } = string.Empty;
     public string Surname { get; set; } = string.Empty;
-    public string Email { get ;set ; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public List<Guid> SchoolGUIDs { get; set; } = new List<Guid>();
 

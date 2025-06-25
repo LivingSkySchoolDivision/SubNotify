43d46809325723e552e403600ff8c0ab64e8ac15
Mark Strendin <mark.strendin@lskysd.ca>
2025-06-25 17:17:48 -0600

    Prevent insane start and end dates from being submitted. If the start date is after the end date, alter the end date to be the start date. If the end date is before the start date, alter the start date to be the end date.

--- src/SubNotify.Core/SubEvent.cs
+++ src/SubNotify.Core/SubEvent.cs
@@ -4,11 +4,37 @@ public class SubEvent : IGUIDable
 {
     public Guid Id { get; set; }
     public DateTime RequestedTimestampUTC { get; set; }
-    public DateTime StartDate { get; set; }
-    public DateTime EndDate { get; set; }
-    public string RequestorOID { get;set; }
-    public string RequestorName { get; set; }    
-    public string RequestorEmail { get; set; }    
+
+    private DateTime _startDate;
+    public DateTime StartDate
+    {
+        get => _startDate;
+        set
+        {
+            this._startDate = value;
+            if (this._startDate > this._endDate)
+            {
+                this._endDate = this._startDate;
+            }
+        }
+    }
+
+    private DateTime _endDate;
+    public DateTime EndDate
+    {
+        get => _endDate;
+        set
+        {
+            this._endDate = value;
+            if (this._endDate < this._startDate)
+            {
+                this._startDate = this._endDate;
+            }
+        }
+    }
+    public string RequestorOID { get; set; }
+    public string RequestorName { get; set; }
+    public string RequestorEmail { get; set; }
     public string SubstituteFor { get; set; }
     public Guid SubGUID { get; set; }
     public string SubName { get; set; }
@@ -18,11 +44,12 @@ public class SubEvent : IGUIDable
     public bool TicketCreated_Onboard { get; set; } = false;
     public bool TicketCreated_Offboard { get; set; } = false;
     public DateTime LastNotifyTimestamp { get; set; }
-    public string Notes { get;set ;}
-    public bool IsCancelled { get; set; } 
+    public string Notes { get; set; }
+    public bool IsCancelled { get; set; }
 
     public override string ToString()
     {
+
         string returnMe = @"
             { 
                 Id: " + this.Id.ToString() + @" 
